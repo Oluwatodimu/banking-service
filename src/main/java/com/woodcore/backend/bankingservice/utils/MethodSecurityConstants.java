2d875ac77fe3d721b3bb0cf726b30c827d9863a2
todimuisewon <todimuisewon@gmail.com>
2024-03-19 10:28:41 +0100

    cleaned up code

--- src/main/java/com/woodcore/backend/bankingservice/utils/MethodSecurityConstants.java
+++ src/main/java/com/woodcore/backend/bankingservice/utils/MethodSecurityConstants.java
@@ -5,6 +5,4 @@ public class MethodSecurityConstants {
     public static final String ADMIN = "hasRole('ROLE_ADMIN')";
 
     public static final String USER = "hasRole('ROLE_USER')";
-
-    public static final String USER_AND_MERCHANT_ROLES = "hasAnyRole('ROLE_USER', 'ROLE_MERCHANT')";
 }
